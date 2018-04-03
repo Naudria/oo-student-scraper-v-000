bc4d25926aece9eed0a01a50d2421e819377e9bf
Rima Jean <rima.jean@gmail.com>
2018-04-03 13:51:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,8 +19,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
-      web_addresses = profile_page.css(".social-icon-container").children.css('a').map{
-        |el| el.attribute('href').value}
+      web_addresses = profile_page.css(".social-icon-container").children.css('a').map{|el| el.attribute('href').value}
         web_addresses.each do |link|
           if link.match(/(twitter)/)
             student[:twitter] = link
